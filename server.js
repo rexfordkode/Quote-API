860727131b45bd126d34d0f3524cdc664976005e
rexfordkode <86994591+rexfordkode@users.noreply.github.com>
2021-12-22 14:52:38 +0000

    Initial commit

---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+
+const { quotes } = require('./data');
+const { getRandomElement } = require('./utils');
+
+const PORT = process.env.PORT || 4001;
+
+app.use(express.static('public'));
+
+app.get('/api/quotes/random', (req, res, next) =>{
+  const quote = getRandomElement(quotes);
+  res.status(200).send({quote: quote});
+});
+
+app.get('/api/quotes',(res,req, next) =>{
+
+  if(req.query.person){
+  const author = req.query.person;
+  const result = quotes.filter(quote => {
+    return quote['person'] === author;
+  });
+  if(result.lenght > 0){
+    res.status(200).send({quotes: result});
+  } else {
+    res.status(200).send({quotes:[]});
+  }
+ }
+  else{
+    res.status(200).send({quotes: quotes});
+  }
+});
+
+app.post("/api/quotes", (req, res, next) => {
+  if (req.query.quote && req.query.person) {
+    const newQuote = {
+      quote: req.query.quote,
+      person: req.query.person,
+    };
+    quotes.push(newQuote);
+    res.status(200).send({ quote: newQuote });
+  } else {
+    res.status(400);
+  }
+});
+
+app.listen(PORT, () =>{
+  console.log(`Server is up on port ${PORT}`);
+});
